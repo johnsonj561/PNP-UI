a978077cf8bace26c34775eb200c33a5c290be10
Justin Johnson <johnsonj561@gmail.com>
2016-09-25 23:10:41 -0400

    Added slider to let user adjust step size

--- src/manual_control_ui/controller/ManualController.java
+++ src/manual_control_ui/controller/ManualController.java
@@ -6,6 +6,7 @@ import javax.swing.JPanel;
 import connection_settings_ui.view.SettingsTitleView;
 import manual_control_ui.view.HomeButtonView;
 import manual_control_ui.view.JogButtonView;
+import manual_control_ui.view.JogSizeSlider;
 import manual_control_ui.view.ManualInstructionView;
 
 /**
@@ -33,6 +34,9 @@ public class ManualController extends JPanel{
 		jogTitle.setAlignmentX(JPanel.LEFT_ALIGNMENT);
 		jogButtonView = new JogButtonView();
 		jogButtonView.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+		//Step size control
+		jogSizeSliderView = new JogSizeSlider("Step Size");
+		jogSizeSliderView.setAlignmentX(JPanel.LEFT_ALIGNMENT);
 		//Home controls
 		homeTitle = new SettingsTitleView("Home Machine");
 		homeTitle.setAlignmentX(JPanel.LEFT_ALIGNMENT);
@@ -50,13 +54,37 @@ public class ManualController extends JPanel{
 		this.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
 		this.add(jogTitle);
 		this.add(jogButtonView);
+		this.add(jogSizeSliderView);
 		this.add(homeTitle);
 		this.add(homeButtonView);
 		this.add(manualInstructionTitle);
 		this.add(manualInstructionView);
 	}
 
+	/**
+	 * Get step size defined by user
+	 * @return int step size
+	 */
+	public int getStepSize(){
+		return jogSizeSliderView.getStepSizeValue();
+	}
 
+	/**
+	 * Update slider with new step size for jog control
+	 * @param int step size
+	 */
+	public void setStepSize(int step){
+		jogSizeSliderView.setStepSizeValue(step);
+	}
+	/**
+	 * Get instruction input entered by user
+	 * @return String G Code instruction entered by user
+	 */
+	public String getManualInstruction(){
+		return manualInstructionView.getInstructionText();
+	}
+	
+	
 	/**
 	 * Default Serial Version UID
 	 */
@@ -65,9 +93,10 @@ public class ManualController extends JPanel{
 	/**
 	 * Class variables
 	 */
-	private JogButtonView jogButtonView;
-	private HomeButtonView homeButtonView;
-	private ManualInstructionView manualInstructionView;
+	public JogButtonView jogButtonView;
+	public JogSizeSlider jogSizeSliderView;
+	public HomeButtonView homeButtonView;
+	public ManualInstructionView manualInstructionView;
 	private SettingsTitleView manualInstructionTitle;
 	private SettingsTitleView jogTitle;
 	private SettingsTitleView homeTitle;
