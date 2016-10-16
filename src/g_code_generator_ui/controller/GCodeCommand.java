72482db315b2485f231b9dc6a7590d4f1054615b
Justin Johnson <johnsonj561@gmail.com>
2016-10-16 19:13:21 -0400

    GCodeCommand updated with regex string matching to detect valid GCode and PIC return values

--- src/g_code_generator_ui/controller/GCodeCommand.java
+++ src/g_code_generator_ui/controller/GCodeCommand.java
@@ -1,5 +1,6 @@
 package g_code_generator_ui.controller;
 
+
 /**
  * Class that parses a single G Code instruction and stores instruction values
  * G1 = linear Move (X, Y, Z, R)
@@ -21,8 +22,12 @@ public class GCodeCommand {
 	 * 
 	 * @param instruction String containing G Code command
 	 */
-	public GCodeCommand(String instruction) {
-		//initialize command types to false until we can identify
+	public GCodeCommand(String instruction, double width, double depth, double height) {
+		//store boundaries
+		maxWidth = width;
+		maxDepth = depth;
+		maxHeight = height;
+		//initialize command types to false until we can identify them properly
 		gCommand = false;
 		xMove = false;
 		yMove = false;
@@ -32,64 +37,342 @@ public class GCodeCommand {
 		sDelay = false;
 		fCommand = false;
 		mCommand = false;
-		//split instruction on spaces
-		instructionTokens = instruction.split("\\s+");
-		for(String token : instructionTokens){
-			identifyToken(token);
+		//initialize validCommand and picReturn to false
+		isPicReturn = false;
+		isValidCommand = false;
+		//remove any comments
+		gCodeString = removeComments(instruction);
+		
+		//take quick peek at command using regex to check for basic format requirements
+		//A 'G' or 'M' followed by 1 or more digits, then 1 or more spaces, followed by 0 or 1 of 
+		//X, Y, Z, R, F, P, S, each of which is separated by 0 or more spaces
+		String gCodeRegex = "^[G,M]{1}\\d+\\s*(X[+]?\\d+(\\.\\d+)?)?\\s*(Y[+]?\\d+(\\.\\d+)?)?\\s*" +
+				"(Z[+]?\\d+(\\.\\d+)?)?\\s*(R[+]?\\d+(\\.\\d+)?)?\\s*(P[+]?\\d+(\\.\\d+)?)?\\s*" +
+				"(S[+]?\\d+(\\.\\d+)?)?\\s*(F[+]?\\d+)?\\s*$";
+		
+		//also have to check if it is a PIC return value
+		String picReturnRegex = "^X\\d+\\.\\d+\\s*Y\\d+\\.\\d+\\s*Z\\d+\\.\\d+\\s*$";
+		
+		//if it matches expected G Code Command Format
+		if(gCodeString.matches(gCodeRegex)){
+			isValidCommand = true;
 		}
-		validateInstruction();
-	}
-	
-	private void validateInstruction(){
-	//TODO Validate command
+		//else if it matches expected Pic Return Value
+		else if(gCodeString.matches(picReturnRegex)){
+			isPicReturn = true;
+			System.out.println("Valid Pic Return Found");
+		}
+		//else command must be invalid
+		else{
+			isValidCommand = false;
+			System.out.println("Fundamental Formatting Failed: " + gCodeString);
+		}
+		
+		//if we passed fundamental testing - then dig deeper for more info
+		if(isValidCommand || isPicReturn){
+			instructionTokens = gCodeString.split("\\s+");
+			for(String token : instructionTokens){
+				identifyToken(token);
+			}
+			//validateInstruction() will run additional tests and return false if validation fails
+			isValidCommand = validateInstruction();
+		}
+		
 		
 	}
-	
-	
+
+	/**
+	 * Remove G Code comments denoted by ';' character
+	 * @param instruction
+	 * @return String G code instruction without comments
+	 */
+	private String removeComments(String instruction){
+		if(instruction.contains(";")){
+			instruction = instruction.substring(0, instruction.indexOf(";"));
+			System.out.println(instruction);
+		}
+		return instruction;
+	}
+
+	/**
+	 * Run a set of rules to verify that the G Code instruction is valid.
+	 * @return true if instruction is valid
+	 * 
+	 */
+	private boolean validateInstruction(){
+		//if we found badCommand in identifyTokens method, no need to proceed any further
+		if(!isValidCommand){
+			return false;
+		}
+		//command must contain G1, G4, G28, M10, M11 to be a valid instruction
+		//if it doesn't contain a G or M command, it's a bad command
+		if(!(isgCommand() || ismCommand())){
+			System.out.println("Must contain either a G or a M Command");
+			return false;
+		}
+		//if it is a G command
+		if(isgCommand()){
+			//1st token must contain the G command
+			if(instructionTokens[0].charAt(0) != 'G'){
+				System.out.println("1st char not G");
+				return false;
+			}
+			//make sure there is only 1 'G' and there are no 'M's
+			for(int i = 1; i < instructionTokens.length; i++){
+				//there can only be 1 G character in string
+				if(instructionTokens[i].contains("G")){
+					System.out.println("Too many Gs");
+					return false;
+				}
+				//there can not be a M command anywhere
+				if(instructionTokens[i].contains("M")){
+					System.out.println("Can't contain G and M");
+					return false;
+				}
+			}
+			//if G28 command, there should be no values for X Y Z R or F
+			if(gValue == 28){
+				if(xMove || yMove || zMove || rMove || fCommand){
+					System.out.println("G28 can not have a value for X Y Z R F");
+					return false;
+				}
+			}
+			//if a G4 command there must be 1 and only 1 of P or S values and they must be > 0
+			if(gValue == 4){
+				//pDelay XNOR sDelay - must have only 1 or the other P or S
+				if(!(pDelay ^ sDelay)){
+					System.out.println("pDelay" + pDelay);
+					System.out.println("sDelay" + sDelay);
+				
+					System.out.println("G4 must have only 1 delay associated with it, P or S");
+					return false;
+				}
+				//make sure delays are greater than 0
+				if(pDelay && pValue < 0){
+					System.out.println("P Delay must be greater than 0");
+					return false;
+				}
+				//make sure delays are greater than 0
+				if(sDelay && sValue < 0){
+					System.out.println("S delay must be greater than 0");
+					return false;
+				}
+			}
+			//if G1 then there must be at least 1 of X Y Z R or F
+			if(gValue == 1){
+				//must be at least one of the X Y Z R or F
+				if(!(xMove || yMove || zMove || rMove || fCommand)){
+					System.out.println("must be at least one X Y Z R F associated with a G1");
+					return false;
+				}
+				if(xMove || yMove){
+					//z move and r move can't exist with x or y. x and y moves can co-exist
+					if(zMove || rMove){
+						System.out.println("Can't have X Y with a Z or R");
+						return false;
+					}
+				}
+				//r moves and z moves must not exist with x y z or each other
+				if(rMove && zMove){
+					System.out.println("Can't have an R and Z together");
+					return false;
+				}
+				//r moves should not be associated with a feed rate
+				if(rMove && fCommand){
+					System.out.println("Can't have an R and F together");
+					return false;
+				}
+			}
+		}
+
+		//If instruction is an M command, validate
+		//Note - M10/M11 is Vacuum On/Off instructions
+		if(ismCommand()){
+			//1st token must contain the M command
+			if(instructionTokens[0].charAt(0) != 'M'){
+				System.out.println("1st char 'M' expected but not found");
+				return false;
+			}
+			//make sure there is only 1 'M' and there are no 'G's
+			//we start at 1 because we want t skip the first token
+			for(int i = 1; i < instructionTokens.length; i++){
+				//there can only be 1 M character in string
+				if(instructionTokens[i].contains("M")){
+					System.out.println("Contains more than 1 M");
+					return false;
+				}
+				//there can not be a G command anywhere
+				if(instructionTokens[i].contains("G")){
+					System.out.println("Contains M and G");
+					return false;
+				}
+			}
+			//there can be no X Y Z R or F
+			if(xMove || yMove || zMove || rMove || fCommand || sDelay || pDelay){
+				System.out.println("there can't be an X Y Z R S P or F associated with a M");
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Identify the command stored within token. If invalid command is found, set validComman flag to false
+	 * Identifiable Tokens include allowable characters (G, M, X, Y, Z, R, F, P, S) followed by appropriate digits
+	 * @param token G Code token to be parsed
+	 */
 	private void identifyToken(String token){
+		String temp = "";
 		char c = token.charAt(0);
+		//switch on the 1st character of token
+		//if 1st character does not match any expected cases, validCommand = false
 		switch(c) {
+		//Case G should only accept values of G equal to 1, 4, or 28
 		case 'G':
-			gValue = Integer.parseInt(token.replace("G", ""));
-			gCommand = true;
+			//remove the G check that value for G is valid then assign to gValue
+			temp = token.replace("G", "");
+			//\\d{1,2} checks for 1 or 2 digits, covers cases G1, G4, G28
+			if(temp.matches("^\\d{1,2}$")){
+				gValue = Integer.parseInt(temp);
+				if(gValue == 1 || gValue == 4 || gValue == 28){
+					gCommand = true;
+				}
+				else{
+					System.out.println("G value expects a 1, 4, or 28");
+					isValidCommand = false;
+				}
+			}
+			else{
+				System.out.println("G value expects 1 or 2 digits");
+				isValidCommand = false;
+			}
 			break;
+			//Case M should only accept values of M equal to 10 or 11 for vacuum on/off
 		case 'M':
-			mValue = Integer.parseInt(token.replace("M", ""));
-			mCommand = true;
+			temp = token.replace("M",  "");
+			//\\d{2} checks for 2 digits, covers cases M10 and M11
+			if(temp.matches("^\\d{2}$")){
+				mValue = Integer.parseInt(temp);
+				if(mValue == 10 || mValue == 11){
+					mCommand = true;
+				}
+				else{
+					System.out.println("M value expects a 10 or 11");
+					isValidCommand = false;
+				}
+			}
+			else{
+				System.out.println("M value expects exactly 2 digits");
+				isValidCommand = false;
+			}
 			break;
+		//X value expects digits and optional decimal
 		case 'X':
-			xValue = Double.parseDouble(token.replace("X", ""));
-			xMove = true;
+			//regex = 0 or 1 '+' signs, 1 or more digits,
+			//^ is beginning of string
+			// [+]? is 0 or 1 plus symbol
+			//\\d+ is 1 or more digits
+			//(\\.\\d+)? is 0 or 1 of the substrings found within parenthesis
+			//\\.\\d+ is a decimal point followed by 1 or more digits
+			//$ is end of line
+			temp = token.replace("X", "");
+			if(temp.matches("^[+]?\\d+(\\.\\d+)?$")){
+				xValue = Double.parseDouble(temp);
+				xMove = true;
+				if(xValue < 0 || xValue > maxWidth){
+					isValidCommand = false;
+				}
+			}
+			else{
+				System.out.println("X expects value that contains digits and optional decimal");
+				isValidCommand = false;
+			}
 			break;
+		//Y value expects digits and optional decimal
 		case 'Y':
-			yValue = Double.parseDouble(token.replace("Y", ""));
-			yMove = true;
+			temp = token.replace("Y", "");
+			if(temp.matches("^[+]?\\d+(\\.\\d+)?$")){
+				yValue = Double.parseDouble(temp);
+				yMove = true;
+				if(yValue < 0 || yValue > maxDepth){
+					isValidCommand = false;
+				}
+			}
+			else{
+				System.out.println("Y expects value that contains digits and optional decimal");
+				isValidCommand = false;
+			}
 			break;
+		//Z value expects digits and optional decimal
 		case 'Z':
-			zValue = Double.parseDouble(token.replace("Z",""));
-			zMove = true;
+			temp = token.replace("Z", "");
+			if(temp.matches("^[+]?\\d+(\\.\\d+)?$")){
+				zValue = Double.parseDouble(temp);
+				zMove = true;
+				if(zValue < 0 || zValue > maxHeight){
+					isValidCommand = false;
+				}
+			}
+			else{
+				System.out.println("Z expects value that contains digits and optional decimal");
+				isValidCommand = false;
+			}
 			break;
+		//R value expects digits and optional decimal
 		case 'R':
-			rValue = Double.parseDouble(token.replace("R",""));
-			rMove = true;
+			temp = token.replace("R", "");
+			if(temp.matches("^[+]?\\d+(\\.\\d+)?$")){
+				rValue = Double.parseDouble(temp);
+				rMove = true;
+			}
+			else{
+				System.out.println("R expects value that contains digits and optional decimal");
+				isValidCommand = false;
+			}
 			break;
+		//P value expects digits and optional decimal
 		case 'P':
-			pValue = Double.parseDouble(token.replace("P",""));
-			pDelay = true;
+			temp = token.replace("P", "");
+			if(temp.matches("^[+]?\\d+(\\.\\d+)?$")){
+				pValue = Double.parseDouble(temp);
+				pDelay = true;
+			}
+			else{
+				System.out.println("P expects value that contains digits and optional decimal");
+				isValidCommand = false;
+			}
 			break;
+		//S value expects digits and optional decimal
 		case 'S':
-			sValue = Double.parseDouble(token.replace("S",""));
-			sDelay = true;
+			temp = token.replace("S", "");
+			if(temp.matches("^[+]?\\d+(\\.\\d+)?$")){
+				sValue = Double.parseDouble(temp);
+				sDelay = true;
+			}
+			else{
+				System.out.println("S expects value that contains digits and optional decimal");
+				isValidCommand = false;
+			}
 			break;
+		//F value expects digits only, will not accept decimal
 		case 'F':
-			fValue = Integer.parseInt(token.replace("F",""));
-			fCommand = true;
+			temp = token.replace("F", "");
+			//accepts an optional plus sign followed by 1 or more digits
+			if(temp.matches("^[+]?\\d+$")){
+				fValue = Integer.parseInt(temp);
+				fCommand = true;
+			}
+			else{
+				System.out.println("F expects a value that contains 1 or more digits only");
+				isValidCommand = false;
+			}
 			break;
+		//Default is only reached if token does not match any of expected values, set validCommand flag to false
 		default:
-			badCommand = true;
+			isValidCommand = false;
 		}
 	}
-	
+
 	/**
 	 * Get copy of instruction tokens
 	 * @return String[] of G Code Tokens
@@ -137,7 +420,7 @@ public class GCodeCommand {
 	public double getzValue() {
 		return zValue;
 	}
-	
+
 	/**
 	 * Get R rotation value
 	 * @return double r rotation value
@@ -153,7 +436,7 @@ public class GCodeCommand {
 	public double getpValue() {
 		return pValue;
 	}
-	
+
 	/**
 	 * get S Value (delay in s)
 	 * @return double S delay in s
@@ -200,7 +483,7 @@ public class GCodeCommand {
 	public boolean iszMove() {
 		return zMove;
 	}
-	
+
 	/**
 	 * Returns true if instruction contains a R value
 	 * @return boolean rMove
@@ -232,7 +515,7 @@ public class GCodeCommand {
 	public boolean isfCommand() {
 		return fCommand;
 	}
-	
+
 	/**
 	 * Returns true if instruction contains M Command
 	 * @return boolean mCommand
@@ -241,25 +524,39 @@ public class GCodeCommand {
 		return mCommand;
 	}
 
-	
 	/**
 	 * Returns true if G Code command is not valid
 	 * @return true if command is invalid or undetected
 	 */
 	public boolean isBadCommand(){
-		return badCommand;
+		return !isValidCommand;
 	}
-	
+
 	/**
 	 * Returns true if G Code command is valid
 	 * @return true if command is valid
 	 */
 	public boolean isValidCommand(){
-		return !badCommand;
+		return isValidCommand;
 	}
+
+	/**
+	 * Returns true if command is consistent with PIC return values
+	 * @return true if this command is consistent with PIC return values
+	 */
+	public boolean isPicReturnValue(){
+		return isPicReturn;
+	}
+
+	public void printCommand(){
+		System.out.println(gCodeString);
+	}
+
 
 	//store input g code instruction as array of tokens
 	private String[] instructionTokens;
+	//boolean - true if matches PIC return value
+	private boolean isPicReturn;
 	//store values
 	private int gValue;
 	private int mValue;
@@ -270,6 +567,7 @@ public class GCodeCommand {
 	private double pValue;
 	private double sValue;
 	private int fValue;
+	private String gCodeString;
 	//identify instruction types
 	private boolean gCommand;
 	private boolean xMove;
@@ -280,7 +578,11 @@ public class GCodeCommand {
 	private boolean sDelay;
 	private boolean fCommand;
 	private boolean mCommand;
+	//boundaries to check X Y Z against
+	private double maxWidth;
+	private double maxDepth;
+	private double maxHeight;
 	//flag to detect bad instruction
-	private boolean badCommand;
-	
+	private boolean isValidCommand;
+
 }
