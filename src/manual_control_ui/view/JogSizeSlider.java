80029ce29018f12d000ba31ddaf37b40d8e4c7cc
Justin Johnson <johnsonj561@gmail.com>
2016-11-19 20:03:16 -0500

    Updated slider to allow for smaller machine movement

--- src/manual_control_ui/view/JogSizeSlider.java
+++ src/manual_control_ui/view/JogSizeSlider.java
@@ -2,6 +2,7 @@ package manual_control_ui.view;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.util.Hashtable;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -47,10 +48,21 @@ public class JogSizeSlider extends JPanel{
 		stepSizeInput.setMaximumSize(new Dimension(SLIDER_WIDTH, stepSizeInput.getPreferredSize().height + 35));
 		stepSizeInput.setPreferredSize(new Dimension(SLIDER_WIDTH, stepSizeInput.getPreferredSize().height + 35));
 		stepSizeInput.setMinimumSize(new Dimension(SLIDER_WIDTH, stepSizeInput.getPreferredSize().height + 35));
-		stepSizeInput.setMajorTickSpacing(5);
-		stepSizeInput.setMinorTickSpacing(1);
+		stepSizeInput.setMajorTickSpacing(1);
 		stepSizeInput.setPaintTicks(true);
 		stepSizeInput.setPaintLabels(true);
+		//custom labels
+		Hashtable<Integer, JLabel> sliderLabels = new Hashtable<Integer, JLabel>();
+		sliderLabels.put(SLIDER_0_25, new JLabel("0.25"));
+		sliderLabels.put(SLIDER_0_50, new JLabel("0.50"));
+		sliderLabels.put(SLIDER_1, new JLabel("1.0"));
+		sliderLabels.put(SLIDER_5, new JLabel("5"));
+		sliderLabels.put(SLIDER_10, new JLabel("10"));
+		sliderLabels.put(SLIDER_20, new JLabel("20"));
+		sliderLabels.put(SLIDER_50, new JLabel("50"));
+		sliderLabels.put(SLIDER_100, new JLabel("100"));
+		stepSizeInput.setLabelTable(sliderLabels);
+		setStepSizeValue(SLIDER_5);
 		stepSizeInput.setFont(new Font("Serif", Font.ITALIC, 12));
 		inputSliderPanel.add(stepSizeInput);
 		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
@@ -64,17 +76,37 @@ public class JogSizeSlider extends JPanel{
 	 * Get step size value from slider
 	 * @return int Step Size
 	 */
-	public int getStepSizeValue(){
-		return stepSizeInput.getValue();
+	public double getStepSizeValue(){
+		switch(stepSizeInput.getValue()){
+		case SLIDER_0_25:
+			return 0.25;
+		case SLIDER_0_50:
+			return 0.50;
+		case SLIDER_1:
+			return 1.0;
+		case SLIDER_5:
+			return 5.0;
+		case SLIDER_10:
+			return 10.0;
+		case SLIDER_20:
+			return 20.0;
+		case SLIDER_50:
+			return 50.0;
+		case SLIDER_100:
+			return 100.0;
+		default:
+			return 0.0;
+		}
 	}
 
 	/**
 	 * Set step size value
 	 * @param step
 	 */
-	public void setStepSizeValue(int step){
-		stepSizeInput.setValue(step);
+	public void setStepSizeValue(int stepSize){
+		stepSizeInput.setValue(stepSize);
 	}
+	
 	/**
 	 * Default serial UID
 	 */
@@ -90,7 +122,16 @@ public class JogSizeSlider extends JPanel{
 	private String heading;
 	public JButton sendInstructionButton;
 	private final int STEP_MIN = 0;
-	private final int STEP_MAX = 20;
-	private final int STEP_DEF = 10;
+	private final int STEP_MAX = 7;
+	private final int STEP_DEF = 1;
 	private final int SLIDER_WIDTH = 275;
+	public static final int SLIDER_0_25 = 0;
+	public static final int SLIDER_0_50 = 1;
+	public static final int SLIDER_1 = 2;
+	public static final int SLIDER_5 = 3;
+	public static final int SLIDER_10 = 4;
+	public static final int SLIDER_20 = 5;
+	public static final int SLIDER_50 = 6;
+	public static final int SLIDER_100 = 7;
+	
 }
