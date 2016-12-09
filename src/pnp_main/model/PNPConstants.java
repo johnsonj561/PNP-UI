c54455bae7fbab4eec28d5b9906ad6bff601bb07
Justin Johnson <johnsonj561@gmail.com>
2016-12-08 21:15:13 -0500

    JFrame dimensions updated

--- src/pnp_main/model/PNPConstants.java
+++ src/pnp_main/model/PNPConstants.java
@@ -16,9 +16,8 @@ public class PNPConstants {
 	public static final int STATUS_LOG_FILE_ERROR = 6;
 	public static final int STATUS_EMERGENCY_STOPPED =7;
 	//commands
-	public static final String HOME_X = "G1 X0";
-	public static final String HOME_Y = "G1 Y0";
-	public static final String HOME_Z = "G1 Z0";
+	public static final String HOME_X = "G28 X";
+	public static final String HOME_Y = "G28 Y";
 	public static final String HOME_ALL = "G28";
 	public static final String EMERGENCY_STOP = "!!!";
 	//threshold for Computer vision routine
@@ -27,7 +26,7 @@ public class PNPConstants {
 	public static final int JFRAME_X_LOC = 50;
 	public static final int JFRAME_Y_LOC = 25;
 	//PNP Main's JFrame Preferred Size
-	public static final int PREFERRED_WIDTH = 700;
+	public static final int PREFERRED_WIDTH = 750;
 	public static final int PREFERRED_HEIGHT = 700;
 
 }
