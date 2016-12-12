ae953d84617209d4071a6e262081acbd66d379e0
Justin Johnson <johnsonj561@gmail.com>
2016-12-11 22:38:34 -0500

    Lightbox and vision constants defined

--- src/pnp_main/model/PNPConstants.java
+++ src/pnp_main/model/PNPConstants.java
@@ -5,9 +5,9 @@ package pnp_main.model;
  * @author Justin Johnson
  *
  */
-public class PNPConstants {
+public final class PNPConstants {
 
-	//connection status
+	//connection status messages
 	public static final int STATUS_CONNECTED = 1;
 	public static final int STATUS_DISCONNECTED = 2;
 	public static final int STATUS_CONNECT_ERROR = 3;
@@ -15,14 +15,30 @@ public class PNPConstants {
 	public static final int STATUS_SEND_ERROR = 5;
 	public static final int STATUS_LOG_FILE_ERROR = 6;
 	public static final int STATUS_EMERGENCY_STOPPED =7;
-	//commands
+	//Homing commands
 	public static final String HOME_X = "G28 X";
 	public static final String HOME_Y = "G28 Y";
 	public static final String HOME_ALL = "G28";
+	//emergency stop
 	public static final String EMERGENCY_STOP = "!!!";
+	//lightbox and computer vision
+	public static final String MOVE_TO_LIGHTBOX = "G1 X35 Y35";
+	public static final String LIGHT_ON = "M12";
+	public static final String LIGHT_OFF = "M13";
+	public static final String LIFT_ABOVE_LIGHTBOX = "G1 Z5";
+	public static final String LOWER_TO_LIGHTBOX = "G1 Z3";
+	public static final String LIGHT_BOX_X = "35.0";
+	public static final String LIGHT_BOX_Y = "35.0";
+	//PCB location
+	public static final String PCB_X = "122.5";
+	public static final String PCB_Y = "20.0";
 	//threshold for Computer vision routine
 	public static final double IMAGE_ORIENTATION_THRESHOLD = 1.0;
 	public static final double IMAGE_POSITION_THRESHOLD = 0.5;
+	public static final int LIGHTBOX_HEIGHT = 6;
+	//Vacuum controls
+	public static final String VACUUM_ON = "M10";
+	public static final String VACUUM_OFF = "M11";
 	//PNP Main's JFrame Location
 	public static final int JFRAME_X_LOC = 50;
 	public static final int JFRAME_Y_LOC = 25;
